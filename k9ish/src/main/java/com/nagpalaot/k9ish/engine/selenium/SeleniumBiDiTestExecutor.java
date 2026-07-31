c613738d79dc136e9470c4a2ef3d968f1339da8d
FFelixberto <ffelixberto@gmail.com>
2026-07-31 07:18:10 -0400

    Added implementation of executor using BiDi

---
+++ k9ish/src/main/java/com/nagpalaot/k9ish/engine/selenium/SeleniumBiDiTestExecutor.java
@@ -0,0 +1,219 @@
+package com.nagpalaot.k9ish.engine.selenium;
+
+import java.time.Duration;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.bidi.module.Network;
+import org.openqa.selenium.bidi.network.Header;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import com.nagpalaot.k9ish.engine.BaseTestExecutor;
+
+public class SeleniumBiDiTestExecutor extends BaseTestExecutor {
+
+	public static final String NAME = "name";
+	public static final String ID = "id";
+	public static final String LINK_TEXT = "linkText";
+	public static final String CSS = "css";
+	public static final String CLASSNAME = "className";
+	public static final String XPATH = "xpath";
+	public static final String HEADER = "header";
+	public static final String COOKIE = "cookie";
+	
+	public static final String TARGET_TYPE_TITLE = "TITLE";
+	public static final String TARGET_TYPE_URL = "URL";
+
+	protected static final String EQUALS = "=";
+	protected static final int DEFAULT_WAIT_TIME_SECONDS = 15;
+	
+	static protected Network network;
+	protected boolean chromeHeadless = false;
+	
+	//private Headers headers;
+	private String testUrl;
+	
+	//protected Headers getHeaders() {
+	//	return headers;
+	//}
+
+	//protected void setHeaders(Headers headers) {
+	//	this.headers = headers;
+	//}
+
+	protected String getTestUrl() {
+		return testUrl;
+	}
+
+	protected void setTestUrl(String testUrl) {
+		this.testUrl = testUrl;
+	}
+
+	public boolean hasDriver() {
+		return driver != null;
+	}
+
+	public boolean isChromeHeadless() {
+		return chromeHeadless;
+	}
+
+	public void setChromeHeadless(boolean chromeHeadless) {
+		this.chromeHeadless = chromeHeadless;
+	}
+	
+	@Override
+	public void initialize() throws Exception {
+		ChromeOptions options = new ChromeOptions();
+		
+		if(chromeHeadless) {
+			options.addArguments("--headless=new");
+			options.addArguments("--window-size=1920,1080");
+		} else {
+			options.addArguments("--start-maximized");
+		}
+		options.enableBiDi();
+		System.out.println("Initialize driver");
+		driver = new ChromeDriver(options);
+		// not using Selenium hub so not using RemoteWebDriver
+		//driver = new RemoteWebDriver(new URL("https:://www.bls.gov"), options);
+		//driver = new Augmenter().augment(driver);
+		network = new Network(driver);
+		//network.addIntercept(new AddInterceptParameters(InterceptPhase.RESPONSE_STARTED));
+		
+		network.onResponseCompleted(responseReceived -> {
+			System.out.println("---------------------");
+			System.out.println(responseReceived.getRequest().getUrl() + " : " + this.getTestUrl());
+			System.out.println("---------------------");
+			if(responseReceived.getRequest().getUrl().equalsIgnoreCase(this.getTestUrl())) {
+				List<Header> headers = responseReceived.getResponseData().getHeaders();
+				Map<String, String> headerMap = getHeaderMap(headers);
+				System.out.println(headerMap);
+			}
+			
+		});
+		
+        setSuccessful(true);
+	}
+	
+	protected Map<String, String> getHeaderMap(List<Header> headerList){
+		Map<String, String> result = new HashMap<String, String>();
+		for(Header header : headerList) {
+			result.put(header.getName(), header.getValue().getValue());
+		}
+		return result;
+	}
+
+	@Override
+	public void shutdown() {
+		if(hasDriver()) {
+			driver.quit();
+		}
+	}
+
+	@Override
+	protected void handleOpenCommand(String url) {
+		System.out.println("Open URL " + url);
+		this.setTestUrl(url);
+		driver.get(url);
+	}
+
+	@Override
+	protected void handleAssertPresentCommand(String target, String expectedValue) {
+		/*
+		 * repurposing the method to also be able to process looking for a header, 
+		 * maybe a cookie later, along with processing looking for a regular element.
+		 */
+		if(target.startsWith(HEADER)) {
+			System.out.println("Find a header check");
+			String headerTarget = target.split(EQUALS)[1];
+			handleAssertPresentHeaderCommand(headerTarget, expectedValue);
+		}
+		else {
+			System.out.println("Find a regular element check");
+			WebElement element = driver.findElement(locatorForTarget(target));
+			if(!commonAssertContain(element, target, expectedValue)) {
+				setSuccessful(false);
+			}
+		}
+	}
+	
+	/**
+	 * Better solution is to add a new command using this method to specifically 
+	 * check for headers.  It requires a listener which puts the headers info 
+	 * into the headers variable.
+	 * This also requires use of Selenium DevTools which works best with Chromium 
+	 * based browsers (and associated webdrivers) but should also work with RemoteWebDriver.
+	 */
+	@Override
+	protected void handleAssertPresentHeaderCommand(String target, String expectedValue) {
+		/*System.out.println("Check headers " + headers);
+		if(headers == null || !headers.containsKey(target)){
+			System.out.println("Header not found.");
+			setSuccessful(false);
+		}
+		else {
+			String actualValue = (String) headers.get(target);
+			System.out.println("Compare expected value: " + expectedValue + " with actual value: " + actualValue);
+			if(!expectedValue.equalsIgnoreCase(actualValue.toLowerCase())) {
+				System.out.println("Expected value not found");
+				setSuccessful(false);
+			}
+		}*/
+	}
+
+	@Override
+	protected void handleWaitThenAssertPresentCommand(String target, String expectedValue) {
+		WebElement element = findElementWithWait(target, DEFAULT_WAIT_TIME_SECONDS);
+		if(!commonAssertContain(element, target, expectedValue)) {
+			setSuccessful(false);
+		}
+	}
+
+	protected By locatorForTarget(String target) {
+		if(target.startsWith(NAME)) {
+			return By.name(target.split(EQUALS)[1]);
+		} else if(target.startsWith(CSS)) {
+			return By.cssSelector(target.split(EQUALS)[1]);
+		} else if(target.startsWith(ID)) {
+			return By.id(target.split(EQUALS)[1]);
+		} else if (target.startsWith(LINK_TEXT)) {
+			return By.linkText(target.split(EQUALS)[1]);
+		}
+		return null;
+	}
+	
+	protected WebElement findElementWithWait(String target, int timeOutInSec) {
+		WebElement element = null;
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(timeOutInSec));
+		for(int retry = 0; retry < 2; retry++) {
+			try {
+				element = wait.until(ExpectedConditions.visibilityOfElementLocated(locatorForTarget(target)));
+				break;
+			} catch (Exception ex) {
+				if(retry == 1) {
+					throw new NoSuchElementException(target);
+				}
+			}
+		}
+		return element;
+	}
+	
+	private boolean commonAssertContain(WebElement targetElement, String targetLocation, String expectedText) {
+		boolean textFoundAtTarget = true;
+		if(expectedText == null || expectedText.trim().isEmpty()) {
+			return false;
+		}
+		String actualValue = targetElement.getText();
+		if(actualValue != null && !actualValue.contains(expectedText)) {
+			textFoundAtTarget = false;
+		}
+		return textFoundAtTarget;
+	}
+}
---
+++ k9ish/src/test/java/com/nagpalaot/k9ish/engine/TestBiDiHeaderCheck.java
@@ -0,0 +1,39 @@
+package com.nagpalaot.k9ish.engine;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+
+import com.nagpalaot.k9ish.dashboard.model.TestScript;
+import com.nagpalaot.k9ish.dashboard.model.TestStep;
+import com.nagpalaot.k9ish.engine.selenium.SeleniumBiDiTestExecutor;
+import com.nagpalaot.k9ish.engine.selenium.SeleniumTestExecutor;
+
+public class TestBiDiHeaderCheck {
+
+	private TestExecutor executor;
+	
+	@Test
+	public void checkHeader() throws Exception {
+		executor = new SeleniumBiDiTestExecutor();
+		executor.initialize();
+		TestScript script = prepareTestScript();
+		boolean result = executor.execute(script);
+		executor.shutdown();
+		assertTrue(result);
+	}
+	
+	private TestScript prepareTestScript(){
+		TestScript  result = new TestScript();
+		List<TestStep> steps = new ArrayList<TestStep> ();
+		TestStep step1 = new TestStep(1,"open","/","","");
+		TestStep step2 = new TestStep(2,"assertPresentHeader","content-security-policy","frame-ancestors https://*.bls.gov","");
+		steps.add(step1);
+		steps.add(step2);
+		result.setTestSteps(steps);
+		return result;
+	}
+}
